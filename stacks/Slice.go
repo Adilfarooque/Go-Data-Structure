2d9f011b6d4543e071b3bf2085aaa709492e6a24
Adifarooque <Adilfarq11@gmail.com>
2023-09-23 13:05:31 +0530

    Added Pop Method

--- stacks/Slice.go
+++ stacks/Slice.go
@@ -10,3 +10,13 @@ func (s *Stacks) Push(str string){
 	*s = append(*s, str)
 }
 
+func (s *Stacks)Pop()(string,bool){
+	if s.isEmpty() {
+		return "",false
+	}else {
+		index:=len(*s)-1
+		value:=(*s)[index]
+		*s = (*s)[:index]
+		return value,true
+	}
+}

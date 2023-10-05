fe9df17e4c06a86832f3c72c9b6c93d39bb041f2
Adifarooque <Adilfarq11@gmail.com>
2023-10-05 22:32:08 +0530

    Removed Elements from Queues

--- queues/queuesDocuments.go
+++ queues/queuesDocuments.go
@@ -14,8 +14,18 @@ func (q *Queues)Add(value int)bool{
 		return false
 	}
 	q.Size++
-	q.Data[q.Back]=value
-	q.Back = (q.Back+1)%(capcity-1)
+	q.Data[q.Back] = value
+	q.Back = (q.Back + 1 ) % (capcity - 1)
 	return true
 }
 
+func (q *Queues)Remove()(int,bool){
+	value:=0
+	if q.Size <= 0{
+		return 0,false
+	}
+	q.Size--
+	value = q.Data[q.Front]
+	q.Front = (q.Front + 1) % (capcity - 1)
+	return value,true
+}
